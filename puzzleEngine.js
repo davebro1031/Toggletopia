574ede5cad7d1c18da203fda5d9203de4f5b366a
davebro1031 <dwbroaddus@gmail.com>
2023-07-10 15:51:40 -0500

    add dropdown menu for level difficulty in navbar

--- puzzleEngine.js
+++ puzzleEngine.js
@@ -1,3 +1,42 @@
+const dropdowns = document.querySelectorAll('.dropdown')
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select')
+    const caret = dropdown.querySelector('.caret')
+    const menu = dropdown.querySelector('.menu')
+    const options = dropdown.querySelectorAll('.menu li')
+    const selected = dropdown.querySelector('.selected')
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked')
+        
+        caret.classList.toggle('caret-rotate')
+
+        menu.classList.toggle('menu-open')
+
+    })
+
+    options.forEach(option =>{
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText
+
+            select.classList.remove('select-clicked')
+
+            caret.classList.remove('caret-rotate')
+
+            menu.classList.remove('menu-open')
+
+            options.forEach(option => {
+                option.classList.remove('active')
+            })
+
+            option.classList.add('active')
+        })
+    })
+
+})
+
+
 // Create game maps
 
 // Super Paper Mario map
@@ -95,6 +134,9 @@ function setDifficulty(choice){
 
     newTarget()
 }
+
+// Default to medium difficulty when loading the page
+setDifficulty(gameMapS8)
 
 
 
